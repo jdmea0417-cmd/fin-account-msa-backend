be715fa4a9e5c60421b7ad07b71ed8aa690e9dcc
fractalbarista <fractalbarista@gmail.com>
2026-08-15 11:24:40 +0900

    webmvc 대신 webflux 를 사용하도록 수정 Transaction Service에 대한 route 추가

---
+++ gateway/src/main/java/com/finaccount/gateway/config/RouterConfig.java
@@ -0,0 +1,29 @@
+package com.finaccount.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouterConfig {
+    @Bean
+    public RouteLocator getRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("transaction-service-h2console", route -> route
+                        .path("/transactions/h2-console/**")
+                        .filters(filter -> filter
+                                .rewritePath(
+                                        "/transactions/(?<segment>.*)",
+                                        "/${segment}"
+                                ))
+                        .uri("lb://TRANSACTION-SERVICE")
+                )
+                .route("transaction-service", route -> route
+                        .path("/transactions/**")
+                        .filters(filter -> filter)
+                        .uri("lb://TRANSACTION-SERVICE")
+                )
+                .build();
+    }
+}
